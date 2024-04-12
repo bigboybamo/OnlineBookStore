a1e1ceb1524c9dbd14bdafb966d056dac5a721cd
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2024-04-12 12:50:18 +0100

    fixed issues for code quality

--- OnlineBookStore/DataAccess/AppDbContext.cs
+++ OnlineBookStore/DataAccess/AppDbContext.cs
@@ -5,11 +5,9 @@ namespace OnlineBookStore.DataAccess
 {
     public class AppDbContext : DbContext
     {
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
         public DbSet<Book> Books { get; set;}
         public DbSet<Author> Authors { get; set;}
     }
--- OnlineBookStore/Program.cs
+++ OnlineBookStore/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddScoped<IServiceClass, ServiceClass>();
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
